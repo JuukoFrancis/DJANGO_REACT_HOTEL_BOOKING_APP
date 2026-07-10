93f59dedf8396a0a2a05a83682f68b42db3fcfa3
JuukoFrancis <juukojuniorfrancis@gmail.com>
2026-07-10 16:00:30 +0300

    django commits

---
+++ Room_Booking/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    ROOM_TYPES = [
+        ('suite','Suite'),
+        ('standard','Standard Room'),
+        ('deluxe','Deluxe Room')
+                  ]
+    
+    CURRENCY_TYPES = [
+        ('USD','USD'),
+        ('USD','EUR'),
+        ('UGX','UGX')
+                  ]
+    
+    name = models.CharField(max_length=100, blank=True, default="")
+    type = models.CharField(max_length=100,  choices=ROOM_TYPES)
+    pricePerNIght = models.IntegerField(default=150)
+    currency = models.CharField(default="UGX", max_length=10, choices=CURRENCY_TYPES)
+    maxOccupancy = models.IntegerField(default=1)
+    description = models.TextField(max_length=1000)
+
