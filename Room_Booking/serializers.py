29e75f640a7ea54d82ddc0ccb1517907a88cf0d8
JuukoFrancis <juukojuniorfrancis@gmail.com>
2026-07-21 16:19:17 +0300

    ad configuration space

--- Room_Booking/serializers.py
+++ Room_Booking/serializers.py
@@ -2,4 +2,9 @@ from rest_framework import serializers
 from .models import Room
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
-    
+    name
+    type
+    pricePerNIght
+    currency
+    maxOccupancy
+    description    
